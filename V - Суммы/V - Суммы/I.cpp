49418388dd867f4c5d490aaf4b722486959e6408
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-10-31 16:12:49 +0400

    1245666

---
+++ V - Суммы/V - Суммы/I.cpp
@@ -0,0 +1,15 @@
+﻿//#include <iostream>
+//#include <cmath>
+//int main()
+//{
+//	double S=0, fact=1;
+//	int n;
+//	std::cin >> n;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		fact *= i;
+//		S += (1/fact);
+//	}
+//	std::cout << S;
+//	return 0;
+//}
