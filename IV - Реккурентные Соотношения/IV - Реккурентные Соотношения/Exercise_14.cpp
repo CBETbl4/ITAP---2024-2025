204ca6f3fced2ae3265a6adf45223dd5ce89c657
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-11-28 09:10:50 +0400

    Update Exercise_14.cpp

--- IV - Реккурентные Соотношения/IV - Реккурентные Соотношения/Exercise_14.cpp
+++ IV - Реккурентные Соотношения/IV - Реккурентные Соотношения/Exercise_14.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 int main()
@@ -6,12 +6,11 @@ int main()
 	double b1=2, b2=1, b;
 	int n;
 	cin >> n;
-	for (int i = 3;i<=n;i++)
+	for (int i = 1;i<=n;i++)
 	{
 		b = (2. / 3) * b1 - (1. / 3) * b2 * b2;
 		b1 = b2;
 		b2 = b;
+		cout << "b(" << i << ") = " << b << endl;
 	}
-	cout << "b(" << n << ") = " << b << endl;
 	return 0;
-}
