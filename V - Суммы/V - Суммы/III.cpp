af7483dcb50b2c7e246e6f760a36f8606ead37ee
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-11-28 09:24:45 +0400

    Update III.cpp

--- V - Суммы/V - Суммы/III.cpp
+++ V - Суммы/V - Суммы/III.cpp
@@ -1,16 +1,16 @@
-//#include <iostream>
-//#include <cmath>
-//using namespace std;
-//int main()
-//{
-//	int i=0;
-//	double S=0, a=1.0/(sqrt(3)), e;
-//	cin >> e;
-//	while (a > e)
-//	{
-//		S += a;
-//		i++; a /= sqrt(pow(3, i));
-//	}
-//	cout << "S = " << S;
-//	return 0;
-//}
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int i=0;
+	double S=0, a=1.0/(sqrt(3)), e;
+	cin >> e;
+	while (a > e)
+	{
+		S += a;
+		i++; a /= sqrt(3);
+	}
+	cout << "S = " << S;
+	return 0;
+}
