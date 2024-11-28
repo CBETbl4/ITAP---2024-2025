e3e98916fc0cbd3fe94193084e208b55ad324f05
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-11-28 08:58:09 +0400

    Update 2-nd.cpp

--- II - Функции/Functions/2-nd.cpp
+++ II - Функции/Functions/2-nd.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+float f(float x, int j)
+{
+	float S = 0;
+	for (int i = 2; i <= j; i += 2)
+	{
+	    S+=x*x/j;
+	}
+	return S;
+}
+int main()
+{
+	float x, Summ=0;
+	int n;
+	cin >> x;
+	cin >> n;
+	for (int j = 2; j <= n; j += 2)
+	{
+	    Summ+=f(x,j);
+	}
+	cout << Summ;
+	return 0;
+}
+/*#include <iostream>
+#include <cmath>
+using namespace std;
 float f(float x, int n)
 {
 	float S = 0;
@@ -18,4 +43,4 @@ int main()
 	cin >> n;
 	cout << f(x, n);
 	return 0;
-}
+}*/
