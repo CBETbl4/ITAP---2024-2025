92568e9eefd10f066edef62c3af46002d72de1a2
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-05 09:42:24 +0400

    Update I.cpp

--- VI - Массивы/VI - Массивы/I.cpp
+++ VI - Массивы/VI - Массивы/I.cpp
@@ -17,3 +17,62 @@
 //     else "No";
 //     return 0;
 // }
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main()
+// {
+// 	int n; cout << "n="; cin >> n;
+// 	int *a=new int[n+1], P = 1;
+// 	for (int i = 0; i < n; ++i)
+// 	{
+// 		cout << "a[" << i << "]="; cin >> a[i];
+// 	}
+//     for (int i = 0; i < n; ++i)
+// 	{
+// 		P*=a[i];
+// 	}
+//     if ((P/100)<10) cout<<"Yea";
+//     else "No";
+//     return 0;
+// }
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main()
+// {
+// 	unsigned int n;int P = 1; cout << "n="; cin >> n;
+// 	int** a = new int*[n];
+//     for (unsigned int j = 0; j < n; ++j)
+//     	{for (unsigned int i = 0; i < n; ++i)
+//     	{
+//     		cout << "a[" << i << "]="; cin >> a[i];
+//     	}}
+//     for (unsigned int j = 0; j < n; ++j)
+//         {for (unsigned int i = 0; i < n; ++i)
+//     	{
+//     		P*=a[i];
+//     	}}
+//     if ((P/100)<10) cout<<"Yea";
+//     else "No";
+//     return 0;
+// }
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main()
+
+// 	int n; cout << "n="; cin >> n;
+// 	int a[n][n], P = 1;
+// 	for (int i = 0; i < n; ++i)
+// 	{
+// 		cout << "a[" << i << "]="; cin >> a[i];
+// 	}
+//     for (int i = 0; i < n; ++i)
+// 	{
+// 		P*=a[i];
+// 	}
+//     if ((P/100)<10) cout<<"Yea";
+//     else "No";
+//     return 0;
+// }
