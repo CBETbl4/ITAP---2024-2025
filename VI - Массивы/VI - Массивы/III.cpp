26de7b36428541f31f767739813bbb64ced170bf
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-19 03:40:43 +0400

    45646

--- VI - Массивы/VI - Массивы/III.cpp
+++ VI - Массивы/VI - Массивы/III.cpp
@@ -1,44 +1,44 @@
-#include <iostream>
-#include <vector>
-void fill(int n1, int m1, std::vector<std::vector<int>>& matrix)
-{
-    int currentNumber = 1;
-    for (int n = 0; n < n1; ++n) {
-        if (n % 2 == 0) {
-            for (int m = 0; m < m1; ++m) matrix[n][m] = currentNumber++;
-        }
-        else {
-            for (int m = m1 - 1; m >= 0; --m) matrix[n][m] = currentNumber++;
-        }
-    }
-}
-void transpose(const std::vector<std::vector<int>>& matrix, std::vector<std::vector<int>>& transponed)
-{
-    int n = matrix.size();
-    int m = matrix[0].size();
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) transponed[j][i] = matrix[i][j];
-    }
-}
-void print(const std::vector<std::vector<int>>& matrix)
-{
-    for (const auto& n : matrix) {
-        for (int num : n) std::cout << num << " ";
-        std::cout << std::endl;
-    }
-}
-
-int main() {
-    int n, m;
-    std::cin >> n >> m;
-    if (n > 0) {
-        std::vector<std::vector<int>> matrix(n, std::vector<int>(m));
-        fill(n, m, matrix);
-        std::vector<std::vector<int>> transposed(m, std::vector<int>(n));
-        transpose(matrix, transposed);
-        print(transposed);
-    }
-    else std::cout << "Error" << std::endl;
-
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//void fill(int n1, int m1, std::vector<std::vector<int>>& matrix)
+//{
+//    int currentNumber = 1;
+//    for (int n = 0; n < n1; ++n) {
+//        if (n % 2 == 0) {
+//            for (int m = 0; m < m1; ++m) matrix[n][m] = currentNumber++;
+//        }
+//        else {
+//            for (int m = m1 - 1; m >= 0; --m) matrix[n][m] = currentNumber++;
+//        }
+//    }
+//}
+//void transpose(const std::vector<std::vector<int>>& matrix, std::vector<std::vector<int>>& transponed)
+//{
+//    int n = matrix.size();
+//    int m = matrix[0].size();
+//    for (int i = 0; i < n; ++i) {
+//        for (int j = 0; j < m; ++j) transponed[j][i] = matrix[i][j];
+//    }
+//}
+//void print(const std::vector<std::vector<int>>& matrix)
+//{
+//    for (const auto& n : matrix) {
+//        for (int num : n) std::cout << num << " ";
+//        std::cout << std::endl;
+//    }
+//}
+//
+//int main() {
+//    int n, m;
+//    std::cin >> n >> m;
+//    if (n > 0) {
+//        std::vector<std::vector<int>> matrix(n, std::vector<int>(m));
+//        fill(n, m, matrix);
+//        std::vector<std::vector<int>> transposed(m, std::vector<int>(n));
+//        transpose(matrix, transposed);
+//        print(transposed);
+//    }
+//    else std::cout << "Error" << std::endl;
+//
+//    return 0;
+//}
--- VI - Массивы/VI - Массивы/IV.cpp
+++ VI - Массивы/VI - Массивы/IV.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void fill(int n1, std::vector<std::vector<int>>& matrix)
-{
-    int currentNumber = 1;
-    for (int n = 0; n < n1; ++n) {
-        if (n % 2 == 0) {
-            for (int m = 0; m < n1; ++m) matrix[n][m] = currentNumber++;
-        }
-        else {
-            for (int m = n1 - 1; m >= 0; --m) matrix[n][m] = currentNumber++;
-        }
-    }
-}
 vector<int> twins(const vector<vector<int>>& matrix)
 {
     int n = matrix.size();
     vector<int> r(n, -1);
-    for (int j = 0; j < n; ++j) { // Проходим по каждому столбцу
+    for (int j = 0; j < n; ++j) {
         for (int i = 0; i < n; ++i) { // Проходим по строкам в текущем столбце
             for (int k = 0; k < i; ++k) { // Проверяем все предыдущие элементы в этом столбце
                 if (matrix[i][j] == matrix[k][j]) {
@@ -25,21 +13,26 @@ vector<int> twins(const vector<vector<int>>& matrix)
                     break;
                 }
             }
-            if (r[j] != -1) {
-                break;
-            }
+            if (r[j] != -1) break;
         }
     }
     return r;
 }
-
-int main() {
-    vector<vector<int>> matrix = {
-        {1, 2, 3},
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> matrix(n, vector<int>(n)); 
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) cin >> matrix[i][j];}
+    //запуск через вводимую матрицу
+    /*vector<vector<int>> matrix = {
+        {1, 2, 4},
         {4, 2, 5},
         {1, 6, 3}
-    };
+    };*/
+    //запуск через определённую матрицу
     vector<int> result = twins(matrix);
-    for (int j = 0; j < result.size(); ++j) cout << "Столбец " << j << ": " << result[j] << endl;
+    for (int j = 0; j < result.size(); ++j) cout << "col " << j << ": " << result[j] << endl;
     return 0;
 }
