36370b79a1099c4fc39827e0c0257541f6e27a43
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-19 02:15:11 +0400

    fsdf
    sdf

--- VI - Массивы/VI - Массивы/III.cpp
+++ VI - Массивы/VI - Массивы/III.cpp
@@ -4,12 +4,10 @@ void fill(int n1, int m1, std::vector<std::vector<int>>& matrix)
 {
     int currentNumber = 1;
     for (int n = 0; n < n1; ++n) {
-        if (n % 2 == 0) {
+        if (n % 2 == 0) 
             for (int m = 0; m < m1; ++m) matrix[n][m] = currentNumber++;
-        }
-        else {
-            for (int m = m1 - 1; m >= 0; --m) matrix[n][m] = currentNumber++;
-        }
+        else 
+            for (int m = m1 - 1; m >= 0; --m) matrix[n][m] = currentNumber++;}
     }
 }
 void transpose(const std::vector<std::vector<int>>& matrix, std::vector<std::vector<int>>& transponed)
@@ -17,15 +15,13 @@ void transpose(const std::vector<std::vector<int>>& matrix, std::vector<std::vec
     int n = matrix.size();
     int m = matrix[0].size();
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) transponed[j][i] = matrix[i][j];
-    }
+        for (int j = 0; j < m; ++j) transponed[j][i] = matrix[i][j];}
 }
 void print(const std::vector<std::vector<int>>& matrix)
 {
     for (const auto& n : matrix) {
         for (int num : n) std::cout << num << " ";
-        std::cout << std::endl;
-    }
+        std::cout << std::endl;}
 }
 
 int main() {
@@ -36,8 +32,7 @@ int main() {
         fill(n, m, matrix);
         std::vector<std::vector<int>> transposed(m, std::vector<int>(n));
         transpose(matrix, transposed);
-        print(transposed);
-    }
+        print(transposed);}
     else std::cout << "Error" << std::endl;
 
     return 0;
