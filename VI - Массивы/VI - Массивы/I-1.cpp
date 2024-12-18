12cfcc9470ee1e1451e54a46ce729d5849442c96
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-19 00:54:36 +0400

    fs

--- VI - Массивы/VI - Массивы/I-1.cpp
+++ VI - Массивы/VI - Массивы/I-1.cpp
@@ -1,19 +1,19 @@
- #include <iostream>
- #include <cmath>
- using namespace std;
- int main()
- {
- 	int n; cout << "n="; cin >> n;
- 	int *a=new int[n+1], P = 1;
- 	for (int i = 0; i < n; ++i)
- 	{
- 		cout << "a[" << i << "]="; cin >> a[i];
- 	}
-     for (int i = 0; i < n; ++i)
- 	{
- 		P*=a[i];
- 	}
-     if ((P/100)<10) cout<<"Yea";
-     else "No";
-     return 0;
- }
+/*
+    вводим длину массива, далее определяем его длину через 12 строку.
+    используем long long для предотвращения переполнения int при очень больших числах
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> array(n);
+    for (int i = 0; i < n; ++i) cin >> array[i];
+    long long p = 1;
+    for (int i = 0; i < n; ++i) p *= array[i];
+    if (p >= 100 && p <= 999) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
