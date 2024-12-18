b7489f10d7372c8a22f8cb12ce0bf91b90bfd2af
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-19 01:24:51 +0400

    ое

--- VI - Массивы/VI - Массивы/I-1.cpp
+++ VI - Массивы/VI - Массивы/I-1.cpp
@@ -1,19 +1,19 @@
-/*
-    we enter the length of the array, then we determine its length after line 12.
-    we use long long to prevent int overflow with very large numbers
-*/
-#include <iostream>
-#include <vector>
-using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    vector<int> array(n);
-    for (int i = 0; i < n; ++i) cin >> array[i];
-    long long p = 1;
-    for (int i = 0; i < n; ++i) p *= array[i];
-    if (p >= 100 && p <= 999) cout << "Yes" << endl;
-    else cout << "No" << endl;
-    return 0;
-}
+///*
+//    we enter the length of the array, then we determine its length after line 12.
+//    we use long long to prevent int overflow with very large numbers
+//*/
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    vector<int> array(n);
+//    for (int i = 0; i < n; ++i) cin >> array[i];
+//    long long p = 1;
+//    for (int i = 0; i < n; ++i) p *= array[i];
+//    if (p >= 100 && p <= 999) cout << "Yes" << endl;
+//    else cout << "No" << endl;
+//    return 0;
+//}
--- VI - Массивы/VI - Массивы/II.cpp
+++ VI - Массивы/VI - Массивы/II.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-#include <limits>
-using namespace std;
-int main()
-{
-    int n;
-    cin >> n;
-    double* arrow = new double[n];
-    for (int i = 0; i < n; ++i) cin >> arrow[i];
-    double maxNeg = numeric_limits<double>::lowest();
-    bool test_for_negative = false;
-    for (int i = 0; i < n; ++i) {
-        if (arrow[i] < 0) test_for_negative = true;
-            if (arrow[i] > maxNeg) maxNeg = arrow[i];}
-    if (test_for_negative) cout << maxNeg << endl;
-    else cout << "No negative in massive" << endl;
-    return 0;
-}
+//#include <iostream>
+//#include <limits>
+//using namespace std;
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    double* arrow = new double[n];
+//    for (int i = 0; i < n; ++i) cin >> arrow[i];
+//    double maxNeg = numeric_limits<double>::lowest();
+//    bool test_for_negative = false;
+//    for (int i = 0; i < n; ++i) {
+//        if (arrow[i] < 0) test_for_negative = true;
+//            if (arrow[i] > maxNeg) maxNeg = arrow[i];}
+//    if (test_for_negative) cout << maxNeg << endl;
+//    else cout << "No negative in massive" << endl;
+//    return 0;
+//}
