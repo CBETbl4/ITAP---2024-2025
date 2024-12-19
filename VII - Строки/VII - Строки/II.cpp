40c67b1c408ebfe1edd928b2d766f3a2a195cfb4
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-19 04:46:24 +0400

    afda

---
+++ VII - Строки/VII - Строки/II.cpp
@@ -0,0 +1,14 @@
+﻿//#include <iostream>
+//#include <unordered_map>
+//using namespace std;
+//int main()
+//{
+//    string s;
+//    cin >> s;
+//    unordered_map<char, int> count;
+//    for (char c : s) { //run on string
+//        if (++count[c] == 2) {
+//            cout << c << endl;
+//            break;}}
+//    return 0;
+//}
---
+++ VII - Строки/VII - Строки/III.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//#include <sstream>
+//#include <string>
+//#include <limits>
+//using namespace std;
+//int main()
+//{
+//    string s;
+//    getline(cin, s);
+//    istringstream stream(s);
+//    int number, minNumber = numeric_limits<int>::max();
+//    while (stream >> number) 
+//        if (number < minNumber) minNumber = number;
+//    if (minNumber == numeric_limits<int>::max()) cout << "No numbers found." << endl;
+//    else cout << "Smallest number: " << minNumber << endl;
+//    return 0;
+//}
