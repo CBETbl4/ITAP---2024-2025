dc9f9becc23271b1d05726e4ee292272e2efec3a
CBETbl4 <cbetbl4.xbox@gmail.com>
2024-12-19 04:11:15 +0400

    sdfsd

--- VI - Массивы/VI - Массивы/IV.cpp
+++ VI - Массивы/VI - Массивы/IV.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
-#include <vector>
-using namespace std;
-vector<int> twins(const vector<vector<int>>& matrix)
-{
-    int n = matrix.size();
-    vector<int> r(n, -1);
-    for (int j = 0; j < n; ++j) {
-        for (int i = 0; i < n; ++i) { // Проходим по строкам в текущем столбце
-            for (int k = 0; k < i; ++k) { // Проверяем все предыдущие элементы в этом столбце
-                if (matrix[i][j] == matrix[k][j]) {
-                    r[j] = k;
-                    break;
-                }
-            }
-            if (r[j] != -1) break;
-        }
-    }
-    return r;
-}
-int main()
-{
-    int n;
-    cin >> n;
-    vector<vector<int>> matrix(n, vector<int>(n)); 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) cin >> matrix[i][j];}
-    //запуск через вводимую матрицу
-    /*vector<vector<int>> matrix = {
-        {1, 2, 4},
-        {4, 2, 5},
-        {1, 6, 3}
-    };*/
-    //запуск через определённую матрицу
-    vector<int> result = twins(matrix);
-    for (int j = 0; j < result.size(); ++j) cout << "col " << j << ": " << result[j] << endl;
-    return 0;
-}
+//#include <vector>
+//using namespace std;
+//vector<int> twins(const vector<vector<int>>& matrix)
+//{
+//    int n = matrix.size();
+//    vector<int> r(n, -1);
+//    for (int j = 0; j < n; ++j) {
+//        for (int i = 0; i < n; ++i) { // Проходим по строкам в текущем столбце
+//            for (int k = 0; k < i; ++k) { // Проверяем все предыдущие элементы в этом столбце
+//                if (matrix[i][j] == matrix[k][j]) {
+//                    r[j] = k;
+//                    break;
+//                }
+//            }
+//            if (r[j] != -1) break;
+//        }
+//    }
+//    return r;
+//}
+//int main()
+//{
+//    int n;
+//    cin >> n;
+//    vector<vector<int>> matrix(n, vector<int>(n)); 
+//        for (int i = 0; i < n; ++i) {
+//            for (int j = 0; j < n; ++j) cin >> matrix[i][j];}
+//    //запуск через вводимую матрицу
+//    /*vector<vector<int>> matrix = {
+//        {1, 2, 4},
+//        {4, 2, 5},
+//        {1, 6, 3}
+//    };*/
+//    //запуск через определённую матрицу
+//    vector<int> result = twins(matrix);
+//    for (int j = 0; j < result.size(); ++j) cout << "col " << j << ": " << result[j] << endl;
+//    return 0;
+//}
---
+++ VI - Массивы/VI - Массивы/V.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void insert(vector<int>& arr, int newElement)
+{
+    int maxValue = *max_element(arr.begin(), arr.end());
+    for (auto iterator1 = arr.begin(); iterator1 != arr.end(); ++iterator1) {
+        if (*iterator1 == maxValue) iterator1 = arr.insert(iterator1 + 1, newElement) + 1;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> array(n);
+    for (int i = 0; i < n; ++i) cin >> array[i];
+    //vector<int> array = { 1, 3, 5, 3, 5, 2, 5, 4 }; //проверка
+    insert(array, 99);
+    for (int num : array) cout << num << " ";
+    cout << endl;
+    return 0;
+}
