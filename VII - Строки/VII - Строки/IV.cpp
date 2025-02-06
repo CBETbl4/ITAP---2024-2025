9933948e419ff04cbb19bb9fce6c3f0378735d4e
CBETbl4 <cbetbl4.xbox@gmail.com>
2025-02-06 11:10:38 +0400

    3412

---
+++ VII - Строки/VII - Строки/IV.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+#include <string>
+bool incl_num(string s)
+{
+	for (int i = 0;i <= s.size();i++)
+		if (isdigit(s[i])) return true;
+	return false;
+}
+int main()
+{
+	string str;
+	cin >> str;
+	char m = 0;
+	for (int i = 0;i<=str.size();i++)
+		if (isdigit(str[i]))
+			if (m < str[i]) m = str[i];
+	if (incl_num(str)==false) cout<<"No numbers";
+	else cout << m;
+}
