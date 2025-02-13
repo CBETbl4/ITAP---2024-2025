9e6dab9c891034006d2f0be44820871b773dbbb7
CBETbl4 <cbetbl4.xbox@gmail.com>
2025-02-13 10:38:07 +0400

    24

--- VIII - Рекур. Функции/VIII - Рекур. Функции/5-3-2.cpp
+++ VIII - Рекур. Функции/VIII - Рекур. Функции/5-3-2.cpp
@@ -1,26 +1,15 @@
-﻿#include <iostream>
-using namespace std;
-unsigned int nod(unsigned int a, unsigned int b)
-{
-    if (a < b) {
-        swap(a, b);
-    }
-    while (a % b != 0) {
-        a = a % b;
-        swap(a, b);
-    }
-    return b;
-}
-unsigned int nok(unsigned int a, unsigned int b) 
-{
-    return (a * b) / nod(a, b);
-}
-int main()
-{
-    int a, b, c, d;
-    cin >> a >> b >> d >> c ;
-    a = a / nod(b, a); b = b / nod(b, a);
-    c = c / nod(c, d); d = d / nod(c, d);
-    a = a * (nok(b, c) / c); d = d * (nok(b, c) / b);
-    cout << a + d << '/' << nok(b, c);
-}
+﻿//#include <iostream>
+//using namespace std;
+//int nod(int a, int b)
+//{
+//    return b == 0 ? a:nod(b, a % b);
+//}
+//int main()
+//{
+//    int a, b, c, d;
+//    cin >> a >> b >> d >> c ;
+//    int nm = a * c + d * b;
+//    int dm = b * c;
+//    int cd = nod(nm,dm);
+//    cout << nm / cd << '/' << dm / cd;
+//}
