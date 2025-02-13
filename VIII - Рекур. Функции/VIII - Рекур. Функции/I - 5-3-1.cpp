e51ed01b98c7519df9ecd2237f30e932989ee5d8
CBETbl4 <cbetbl4.xbox@gmail.com>
2025-02-13 11:34:49 +0400

    1232421

---
+++ VIII - Рекур. Функции/VIII - Рекур. Функции/I - 5-3-1.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//using namespace std;
+//int nod(int a, int b) {
+//    if (a < b) {
+//        swap(a, b);
+//    }
+//    while (b) {
+//        a %= b;
+//        swap(a, b);
+//    }
+//    return a;
+//}
+//int main()
+//{
+//    int a, b, c, d;
+//    cin >> a >> b >> d >> c;
+//    int nm = a * c + d * b;
+//    int dm = b * c;
+//    int cd = nod(nm, dm);
+//    cout << nm / cd << '/' << dm / cd;
+//}
---
+++ VIII - Рекур. Функции/VIII - Рекур. Функции/II.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//int counter(const string &f)
+//{
+//	int i = 0;
+//	for (char c : f)
+//		if (isdigit(c))
+//			i++;
+//	return i;
+//}
+int i = 0;
+int counter(const string& f, int a) {
+	if (a >= 0) { if (isdigit(f[a])) i++; }
+	else {
+		return i; i = 0;
+	}
+	a = a - 1;
+	counter(f, a);
+}
+int main()
+{
+	string sent1, sent2;
+	cin >> sent1 >> sent2;
+	if (counter(sent1,sent1.size()) > counter(sent2, sent2.size()))
+		(cout << "Sentence 1 has more digs than Sentence 2") << endl;
+	else (cout << "Sentence 2 has more digs than Sentence 1") << endl;
+	cout << counter(sent1, sent1.size()) << endl << counter(sent2, sent2.size()) << endl;
+	i = 0;
+}
