4847f0212113e10e8a27e79cbb7ee1688b2b2216
CBETbl4 <cbetbl4.xbox@gmail.com>
2025-02-27 00:41:34 +0400

    215

--- VIII - Рекур. Функции/VIII - Рекур. Функции/II.cpp
+++ VIII - Рекур. Функции/VIII - Рекур. Функции/II.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
-#include <string>
-using namespace std;
-//int counter(const string &f)
-//{
-//	int i = 0;
-//	for (char c : f)
-//		if (isdigit(c))
-//			i++;
-//	return i;
+//#include <iostream>
+//#include <string>
+//using namespace std;
+////int counter(const string &f)
+////{
+////	int i = 0;
+////	for (char c : f)
+////		if (isdigit(c))
+////			i++;
+////	return i;
+////}
+//int i = 0;
+//int counter(const string& f, int a) {
+//	if (a >= 1) { if (isdigit(f[a-1])) i++; }
+//	else {
+//		int j = i;
+//		i = 0;
+//		return j;
+//	}
+//	a = a - 1;
+//	counter(f, a);
 //}
-int i = 0;
-int counter(const string& f, int a) {
-	if (a >= 1) { if (isdigit(f[a-1])) i++; }
-	else {
-		int j = i;
-		i = 0;
-		return j;
-	}
-	a = a - 1;
-	counter(f, a);
-}
-int main()
-{
-	string sent1, sent2;
-	cin >> sent1 >> sent2;
-	if (counter(sent1,sent1.size()) > counter(sent2, sent2.size()))
-		(cout << "Sentence 1 has more digs than Sentence 2") << endl;
-	else (cout << "Sentence 2 has more digs than Sentence 1") << endl;
-	cout << counter(sent1, sent1.size()) << endl << counter(sent2, sent2.size()) << endl;
-	i = 0;
-}
+//int main()
+//{
+//	string sent1, sent2;
+//	cin >> sent1 >> sent2;
+//	if (counter(sent1,sent1.size()) > counter(sent2, sent2.size()))
+//		(cout << "Sentence 1 has more digits than Sentence 2") << endl;
+//	else if (counter(sent1, sent1.size()) > counter(sent2, sent2.size()))
+//		(cout << "Sentence 2 has more digits than Sentence 1") << endl;
+//	else (cout << "Sentence 1 and 2 has equal count of digits") << endl;
+//	cout << counter(sent1, sent1.size()) << endl << counter(sent2, sent2.size()) << endl;
+//	i = 0;
+//}
---
+++ VIII - Рекур. Функции/VIII - Рекур. Функции/III.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+void findMaxRecursive(int mass[], int size, int index, int& maxValue, int& maxIndex) {
+    if (index >= size) {
+        return;}
+    if (mass[index] > maxValue) {
+        maxValue = mass[index];
+        maxIndex = index;}
+    findMaxRecursive(mass, size, index + 1, maxValue, maxIndex);
+}
+int main() {
+    int size;
+
+    cout << "Enter a size of array: ";
+    cin >> size;
+
+    int* arr = new int[size];
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];}
+    int maxValue = INT_MIN;
+    int maxIndex = -1;
+    findMaxRecursive(arr, size, 0, maxValue, maxIndex);
+    cout << "Maximam element: " << maxValue << endl;
+    cout << "Index of it: " << maxIndex << endl;
+    return 0;
+}
